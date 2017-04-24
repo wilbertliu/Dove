7817a811ff193e0dcca4752f16b75e65c4c5cc07
Wilbert Liu <wilbertliu@gmail.com>
2017-04-24 16:24:19 +0700

    Add initial project

---
+++ Dove/Dove.h
@@ -0,0 +1,19 @@
+//
+//  Dove.h
+//  Dove
+//
+//  Created by Wilbert Liu on 4/24/17.
+//  Copyright © 2017 Wilbert Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dove.
+FOUNDATION_EXPORT double DoveVersionNumber;
+
+//! Project version string for Dove.
+FOUNDATION_EXPORT const unsigned char DoveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dove/PublicHeader.h>
+
+
